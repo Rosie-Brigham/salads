6e96551ed2257cb208a1baae3d5936521f16c6aa
Rosie-Brigham <rosiebrigham@gmail.com>
2025-02-18 08:16:05 +0000

    adds seeds and better instructions

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,17 +4,9 @@ salads = [
     name: 'Tabbouleh',
     description: 'A lovely fresh and healthy salad that uses mostly herbs with a little bulgur wheat',
     ingredients: '50g bulgur wheat, 50g flat-leaf parsley (chopped), 50g mint (chopped), 200g ripe tomatoes (deseeded and diced), 3 spring onions (finely sliced), juice 1 lemon, 3 tbsp olive oil',
-    total_needed: 10,
+    total_needed: 12,
     image_url: 'https://cdn.loveandlemons.com/wp-content/uploads/2022/08/tabbouleh-1.jpg',
     instructions_url: 'https://docs.google.com/document/d/1H7rZT8yyT5b8VVwoRkcjjsdGzhu8s0TtoU498wMeUIk/edit?usp=sharing'
-  },
-  {
-    name: 'Potato salad',
-    description: 'Please make sure the mayonnaise is vegan!',
-    ingredients: '600g baby new potatoes (halve any larger ones), 2 tsp white wine vinegar, 2 tsp extra virgin olive oil, 120g vegan mayonnaise, 1½ tsp Dijon mustard, 1 shallot or ½ a red onion (finely chopped), 2 tsp capers (chopped if large), small handful of parsley (chopped), small handful of chives (chopped)',
-    total_needed: 8,
-    image_url: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/vegan-potato-salad-bfcb85c.jpg?quality=90&webp=true&resize=440,400',
-    instructions_url: 'https://docs.google.com/document/d/1_GYx3opj3N_udtbYx9A2VFZGABpFnqUnQnEEfnCd5TM/edit?usp=sharing'
   },
   {
     name: 'Roasted sweet potato',
@@ -23,6 +15,14 @@ salads = [
     total_needed: 12,
     image_url: 'https://ottolenghi.co.uk/cdn/shop/files/Roasted_sweet_potato_with_pecan_and_maple.jpg?v=1716838382&width=1000',
     instructions_url: 'https://docs.google.com/document/d/18UWesHnUSjrRTHrJAhexsmTWrdDbLN94h9dygjzn3qM/edit?usp=sharing'
+  },
+  {
+    name: 'Bean salad',
+    description: 'A delicious dense bean salad with all the tasty ingridients. Please make sure the mayonnaise is vegan!',
+    ingredients: '2 can of chickpeas, 2 can of different beans (black or cannelini),2 large red peppers, charred sweetcorn (about 4 cobs), 2 packets tomatoes - chopped small, 5/6 tablespoons chopped parsley, Large handfull pickled gerkins, 2 tablespoons vegan mayonaise, Juice of 2 lemons, 2 tablespoons pickle juice (from the gerkins), 2 teaspoons smoked paprika, 2 tablespoons dill - chopped finely, garlic powerder (or 1 fresh clove), salt & pepper',
+    total_needed: 8,
+    image_url: 'https://www.emmymade.com/wp-content/uploads/2024/09/dense-bean-salad-scaled.jpg',
+    instructions_url: 'https://docs.google.com/document/d/1y05U0qkQRemKwj1uhri0IPTLKNsy6vPnwJF-Vp3GrR8/edit?usp=sharing'
   }
 ]
 
